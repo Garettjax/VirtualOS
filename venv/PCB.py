6d1b537a54576479e0658092c643c9b3dbabad39
Garetjax <bklogan1106@hotmail.com>
2020-02-07 18:36:25 -0500

    Test Commit

---
+++ venv/PCB.py
@@ -0,0 +1,7 @@
+class PCB:
+
+    def __init__(self):
+        self.cpuId = 0
+        self.programCounter = 0
+        self.state = State()
+        
---
+++ venv/State.py
@@ -0,0 +1,6 @@
+
+class State(PCB):
+
+    def __init__(self, jobId, priority):
+        self.jobId = jobId
+        self.priority = priority
