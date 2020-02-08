9837af88c04803287c613b814903bcc52dcc2ecd
Garetjax <bklogan1106@hotmail.com>
2020-02-07 20:12:44 -0500

    Basic Parsing for program_file

---
+++ venv/Loader.py
@@ -0,0 +1,26 @@
+from PCB import PCB
+
+file = open ("program_file")
+
+for line in file:
+    # print(line)
+    if "//" in line:
+        card = line[3: int(len(line) - 1)]
+        pcb = PCB()
+
+        arr = card.split(" ")
+        pcb.type = arr[0]
+        pcb.jobId = arr[1]
+        pcb.numberOfJobs = int("0x" + arr[2], 0)
+        pcb.priority = arr[3]
+
+        # print(card)
+        print(pcb.type)
+        print(pcb.jobId)
+        print(pcb.priority)
+        print(pcb.numberOfJobs)
+    else:
+        print(line)
+
+
+file.close()
